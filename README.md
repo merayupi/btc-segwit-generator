5dc314278fc284425fcd74c0a788db1882a23b95
meracon <bintang555pamungkas@gmail.com>
2023-06-02 10:12:21 +0700

    update

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 # BTC SEGWIT ADDRESS GENERATOR
 
 This is just example to generate segwit using javascript
+
 The output will be address and private key (WIF)
+
+## HOW TO USE
+```
+npm install
+```
+```
+node index.js
+```
+
+It will generate .csv file that contain generated address and private key
+
+base code is in tes.js
+
--- index.js
+++ index.js
@@ -2,22 +2,57 @@ const bip39 = require('bip39');
 const ecc = require('tiny-secp256k1');
 const { BIP32Factory } = require('bip32');
 const bitcoin = require('bitcoinjs-lib');
+const {createWriteStream, existsSync} = require('node:fs')
+const rl = require('readline-sync');
 
+let filename = 'generated.csv';
+let i = 2;
 
-const bip32 = BIP32Factory(ecc);
-let seedPhrase = bip39.generateMnemonic();
+while(existsSync(filename)) {
+  filename = `generated_${i}.csv`;
+  i++;
+}
 
-const seedBuffer = bip39.mnemonicToSeedSync(seedPhrase);
-const root = bip32.fromSeed(seedBuffer);
+let writeStream = createWriteStream(`./${filename}`);
 
-const account = root.derivePath("m/44'/0'/0'");
-const node = account.derivePath("0");
-const child = node.derive(0);
+const main = async () => {
 
-const publicKey = child.publicKey;
+    console.log(`
+    +-----------------------------------------+
+    |
+    | 	BTC Segwit Wallet Generator
+    |   
+    |	
+    +-----------------------------------------+
+            `)
 
-const wallet = bitcoin.payments.p2wpkh({ pubkey: publicKey });
-const privateKey = child.toWIF()
-const address = wallet.address
+    const numberToGenerate = rl.question('[?] How Many Wallet: ')
+    console.log('Generating keys...');
 
-console.log(`Address : ${address}\nPrivate Key : ${privateKey}`)
+    writeStream.write('Address,Private Key\n');
+
+    for(let i = 0; i < numberToGenerate; i++) {
+        const bip32 = BIP32Factory(ecc);
+        let seedPhrase = bip39.generateMnemonic();
+
+        const seedBuffer = bip39.mnemonicToSeedSync(seedPhrase);
+        const root = bip32.fromSeed(seedBuffer);
+
+        const account = root.derivePath("m/44'/0'/0'");
+        const node = account.derivePath("0");
+        const child = node.derive(0);
+
+        const publicKey = child.publicKey;
+
+        const wallet = bitcoin.payments.p2wpkh({ pubkey: publicKey });
+        const privateKey = child.toWIF()
+        const address = wallet.address
+
+        writeStream.write(`${address},${privateKey}${i + 1 == numberToGenerate ? '' : '\n'}`)
+    }
+
+    writeStream.end();
+    console.log(`Wallets generated and saved in ${filename}!`);
+}
+
+main()
